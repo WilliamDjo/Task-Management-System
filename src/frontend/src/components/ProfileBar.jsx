56a3061eee178fa89a33f19f3f716883d9b4d15c
Jonathan Sinani <z5256969@unsw.edu.au>
2023-06-23 23:39:56 +1000

    Created a navigation bar for profile and a basic profile page

---
+++ src/frontend/src/components/ProfileBar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Box, Flex, Heading, Link, Text } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const ProfileBar = (props) => {
+  const navigate = useNavigate();
+  return (
+    <Flex height='100vh' flexFlow='column'>
+      <Box bg='gold'><Heading>Navigation Bar</Heading></Box>
+      <Flex height='100%'>
+        <Box flex={1} bg='cornflowerblue' color='white' padding='10px' fontSize='xl' fontWeight='bold'>
+          <Box bg={ props.pendingTasks ? 'black' : '' } padding='10px'><Text><Link>Pending Tasks</Link></Text></Box>
+          <Box bg={ props.myProfile ? 'black' : '' } padding='10px'><Text><Link onClick={() => navigate('edit')}>My Profile</Link></Text></Box>
+          <Box bg={ props.updateProfile ? 'black' : '' } padding='10px'><Text><Link>Update Profile</Link></Text></Box>
+          <Box bg={ props.connections ? 'black' : '' } padding='10px'><Text><Link>Connections</Link></Text></Box>
+        </Box>
+        <Box flex={4} padding='10px'>{props.children}</Box>
+      </Flex>
+    </Flex>
+  )
+}
+
+ProfileBar.propTypes = {
+  children: PropTypes.element,
+  pendingTasks: PropTypes.bool,
+  myProfile: PropTypes.bool,
+  updateProfile: PropTypes.bool,
+  connections: PropTypes.bool
+}
+
+export default ProfileBar;
--- src/frontend/src/pages/Profile.jsx
+++ src/frontend/src/pages/Profile.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import { Box, Card, CardBody, CardHeader, Flex, Heading, Image, Text } from '@chakra-ui/react';
+import logo from '../logo.svg'
+
+import ProfileBar from '../components/ProfileBar';
 
 const Profile = () => {
   return (
-    <Heading>Your Profile</Heading>
-  );
+    <ProfileBar myProfile>
+      <Heading>My Profile</Heading>
+      <Card>
+        <CardHeader>
+          <Heading fontSize='lg'>Name</Heading>
+        </CardHeader>
+        <CardBody>
+          <Flex>
+            <Image src={logo} borderRadius='3xl' height='100px' width='100px'></Image>
+          <Box padding='10px'>
+            <Text>Email: email@example.com</Text>
+            <Text>Password: **********</Text>
+            <Text>Connections: ??</Text>
+          </Box>
+          </Flex>
+        </CardBody>
+      </Card>
+    </ProfileBar>);
 }
 
 export default Profile;
