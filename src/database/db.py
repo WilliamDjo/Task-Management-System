462294b573c7bcf3264ee98834e69d8efaac80ed
Sanyam Jain <sanyamjain47@gmail.com>
2023-06-24 21:34:20 +1000

    Forgot to add these test functions

--- src/database/db.py
+++ src/database/db.py
@@ -9,9 +9,7 @@ def getDB():
     client = MongoClient(url)
 
     db = client['TaskSystem']
-    print(type(db))
-    print(db.list_collection_names())
-
+    
     return db
 
 def getUserInfoCollection(db: Database) -> Collection:
@@ -185,3 +183,31 @@ def updateUserProfile(email: str, data: dict) -> dict:
 
     # Return a dictionary indicating success
     return {'Success': True, 'Message': 'User updated successfully'}
+
+# FOR TESTING ONLY
+def clear_collection(collection_name: str) -> dict:
+    # Get the database
+    db = getDB()
+
+    # Get the collection object from the database
+    collection = db[collection_name]
+
+    # Delete all documents from the collection
+    collection.delete_many({})
+
+    # Return a dictionary indicating success
+    return {'Success': True, 'Message': f'All documents from {collection_name} deleted successfully'}
+
+def print_all_from_collection(collection_name: str):
+    # Get the database
+    db = getDB()
+
+    # Get the collection object from the database
+    collection = db[collection_name]
+
+    # Find all documents in the collection
+    documents = collection.find()
+
+    # Loop through the documents and print each one
+    for document in documents:
+        print(document)
