16310da21095b68b1d3ab2e9eb61f865ee992069
Jonathan Sinani <z5256969@unsw.edu.au>
2023-06-26 02:39:55 +1000

    System admin delete users screen done (without api calls)

--- src/frontend/src/pages/AdminDashboard.jsx
+++ src/frontend/src/pages/AdminDashboard.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Box, Button, ButtonGroup, Center, Flex, Heading, SimpleGrid, Text } from '@chakra-ui/react';
+import { AlertDialog, AlertDialogBody, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, Box, Button, ButtonGroup, Center, Flex, Heading, SimpleGrid, Text, useDisclosure } from '@chakra-ui/react';
 import NavigationBar from '../components/NavigationBar';
 
 const AdminDashboard = () => {
-  const users = [
+  const [deleteUser, setDeleteUser] = React.useState();
+  const [deleteUserId, setDeleteUserId] = React.useState();
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [users, setUsers] = React.useState([
     {
       name: 'Akshay',
       email: 'akshay@taskmaster.com',
@@ -29,7 +33,20 @@ const AdminDashboard = () => {
       email: 'jonathan@taskmaster.com',
       uid: 7890,
     }
-  ]
+  ]);
+
+  const handleDeleteButton = (user, id) => {
+    setDeleteUser(user);
+    setDeleteUserId(id);
+    onOpen()
+  }
+
+  const handleFinalDelete = () => {
+    const newUsers = [...users];
+    const newUsersFiltered = newUsers.filter((user) => user.uid !== deleteUserId);
+    setUsers(newUsersFiltered);
+    onClose();
+  }
   return (
     <Box minH='100vh' h='100vh'>
       <Flex h='100%' flexFlow='column'>
@@ -44,7 +61,7 @@ const AdminDashboard = () => {
                 <Text>ID: {user.uid}</Text>
                 <Center>
                   <ButtonGroup size='sm' isAttached>
-                    <Button bg={'red.400'} color={'white'} _hover={{ bg: 'blue.500' }}>Delete</Button>
+                    <Button bg={'red.400'} color={'white'} _hover={{ bg: 'blue.500' }} onClick={() => handleDeleteButton(user.name, user.uid)}>Delete</Button>
                     <Button bg={'blue.400'} color={'white'} _hover={{ bg: 'blue.500' }}>Reset Password</Button>
                   </ButtonGroup>
                 </Center>
@@ -53,6 +70,29 @@ const AdminDashboard = () => {
           })}
         </SimpleGrid>
       </Flex>
+      <AlertDialog
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+              Delete {deleteUser}
+            </AlertDialogHeader>
+            <AlertDialogBody>
+              Are you sure? You cannot undo this action afterwards.
+            </AlertDialogBody>
+            <AlertDialogFooter>
+              <Button onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme='red' onClick={handleFinalDelete} ml={3}>
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
     </Box>
   );
 }
