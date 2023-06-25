40b3d084fb99c4d8e3db833c3b29fe6f45d794d8
Jonathan Sinani <z5256969@unsw.edu.au>
2023-06-26 02:22:38 +1000

    Started the admin dashboard screen

--- src/frontend/src/pages/AdminDashboard.jsx
+++ src/frontend/src/pages/AdminDashboard.jsx
@@ -1,13 +1,57 @@
 import React from 'react';
-import { Box, Flex, Heading } from '@chakra-ui/react';
+import { Box, Button, ButtonGroup, Center, Flex, Heading, SimpleGrid, Text } from '@chakra-ui/react';
 import NavigationBar from '../components/NavigationBar';
 
 const AdminDashboard = () => {
+  const users = [
+    {
+      name: 'Akshay',
+      email: 'akshay@taskmaster.com',
+      uid: 1234,
+    },
+    {
+      name: 'Cameron',
+      email: 'cameron@taskmaster.com',
+      uid: 5678,
+    },
+    {
+      name: 'Sanyam',
+      email: 'sanyam@taskmaster.com',
+      uid: 9012,
+    },
+    {
+      name: 'William',
+      email: 'william@taskmaster.com',
+      uid: 3456,
+    },
+    {
+      name: 'Jonathan',
+      email: 'jonathan@taskmaster.com',
+      uid: 7890,
+    }
+  ]
   return (
     <Box minH='100vh' h='100vh'>
       <Flex h='100%' flexFlow='column'>
         <NavigationBar />
         <Heading>Admin Dashboard</Heading>
+        <SimpleGrid columns={[1, 2, 3]} spacing='3' m='3'>
+          {users.map((user, index) => {
+            return (
+              <Box bg='blue.50' key={index} borderRadius='xl' p='2'>
+                <Text as='b'>{user.name}</Text>
+                <Text>{user.email}</Text>
+                <Text>ID: {user.uid}</Text>
+                <Center>
+                  <ButtonGroup size='sm' isAttached>
+                    <Button bg={'red.400'} color={'white'} _hover={{ bg: 'blue.500' }}>Delete</Button>
+                    <Button bg={'blue.400'} color={'white'} _hover={{ bg: 'blue.500' }}>Reset Password</Button>
+                  </ButtonGroup>
+                </Center>
+              </Box>
+            )
+          })}
+        </SimpleGrid>
       </Flex>
     </Box>
   );
