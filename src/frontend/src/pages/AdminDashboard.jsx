2c30aaa947855556742cd7d59235aa0c874aeb30
WilliamDjo <william.djong@yahoo.com>
2023-06-28 05:46:43 +1000

    Added routes for forgot password

--- src/frontend/src/pages/AdminDashboard.jsx
+++ src/frontend/src/pages/AdminDashboard.jsx
@@ -1,5 +1,31 @@
 import React from 'react';
-import { AlertDialog, AlertDialogBody, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, Box, Button, ButtonGroup, Flex, FormControl, FormLabel, Heading, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, SimpleGrid, Stack, Text, useDisclosure, useToast } from '@chakra-ui/react';
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Box,
+  Button,
+  ButtonGroup,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  SimpleGrid,
+  Stack,
+  Text,
+  useDisclosure,
+  useToast,
+} from '@chakra-ui/react';
 import NavigationBar from '../components/NavigationBar';
 import PasswordBar from '../components/PasswordBar/PasswordBar';
 import { fetchBackend } from '../fetch';
@@ -31,57 +57,68 @@ const AdminDashboard = () => {
     {
       username: 'Jonathan',
       email: 'jonathan@taskmaster.com',
-    }
+    },
   ]);
   // const [users, setUsers] = React.useState([]);
 
   React.useEffect(() => {
-    const token = localStorage.getItem('token')
-    fetchBackend('/getallusers', 'POST', { token })
-      .then((data) => {
-        if (data.error) {
-          console.log(data.error);
-        } else {
-          setUsers(data.Data)
-        }
-      })
-  }, [])
+    const token = localStorage.getItem('token');
+    fetchBackend('/getallusers', 'POST', { token }).then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setUsers(data.Data);
+      }
+    });
+  }, []);
 
-  const { isOpen: isAlertOpen, onOpen: onAlertOpen, onClose: onAlertClose } = useDisclosure();
-  const { isOpen: isModalOpen, onOpen: onModalOpen, onClose: onModalClose } = useDisclosure();
+  const {
+    isOpen: isAlertOpen,
+    onOpen: onAlertOpen,
+    onClose: onAlertClose,
+  } = useDisclosure();
+  const {
+    isOpen: isModalOpen,
+    onOpen: onModalOpen,
+    onClose: onModalClose,
+  } = useDisclosure();
   const toast = useToast();
 
   const handleDeleteButton = (user, email) => {
     setSelectedUser(user);
     setSelectedUserEmail(email);
     onAlertOpen();
-  }
+  };
 
   const handleFinalDelete = () => {
-    const token = localStorage.getItem('token')
-    fetchBackend('/admin/delete', 'DELETE', { token, email: selectedUserEmail })
-      .then((data) => {
-        if (data.error) {
-          toast({
-            title: data.error,
-            status: 'error',
-            duration: 5000,
-            isClosable: true,
-          });
-        } else {
-          const newUsers = [...users];
-          const newUsersFiltered = newUsers.filter((user) => user.email !== selectedUserEmail);
-          setUsers(newUsersFiltered);
-          onAlertClose();
-          toast({
-            title: `${selectedUser}'s account successfully deleted.`,
-            status: 'success',
-            duration: 5000,
-            isClosable: true,
-          });
-        }
-      })
-  }
+    const token = localStorage.getItem('token');
+    fetchBackend('/admin/delete', 'DELETE', {
+      token,
+      email: selectedUserEmail,
+    }).then(data => {
+      if (data.error) {
+        toast({
+          title: data.error,
+          status: 'error',
+          duration: 5000,
+          isClosable: true,
+        });
+      } else {
+        const newUsers = [...users];
+        const newUsersFiltered = newUsers.filter(
+          user => user.email !== selectedUserEmail
+        );
+        setUsers(newUsersFiltered);
+        onAlertClose();
+        toast({
+          title: `${selectedUser}'s account successfully deleted.`,
+          status: 'success',
+          duration: 5000,
+          isClosable: true,
+        });
+      }
+    });
+  };
 
   const handleResetButton = (user, email) => {
     setSelectedUser(user);
@@ -89,7 +126,7 @@ const AdminDashboard = () => {
     setNewPassword('');
     setConfirmNewPassword('');
     onModalOpen();
-  }
+  };
 
   const handleFinalPasswordReset = () => {
     if (newPassword !== confirmNewPassword) {
@@ -99,66 +136,80 @@ const AdminDashboard = () => {
         duration: 5000,
         isClosable: true,
       });
-      return
+      return;
     }
-    const token = localStorage.getItem('token')
-    fetchBackend('/admin/reset', 'PUT', { token, email: selectedUserEmail, password: newPassword })
-      .then((data) => {
-        if (data.error) {
-          toast({
-            title: data.error,
-            status: 'error',
-            duration: 5000,
-            isClosable: true,
-          });
-        } else {
-          toast({
-            title: `${selectedUser}'s password successfully reset.`,
-            status: 'success',
-            duration: 5000,
-            isClosable: true,
-          });
-          onModalClose();
-        }
-      })
-  }
+    const token = localStorage.getItem('token');
+    fetchBackend('/admin/reset', 'PUT', {
+      token,
+      email: selectedUserEmail,
+      password: newPassword,
+    }).then(data => {
+      if (data.error) {
+        toast({
+          title: data.error,
+          status: 'error',
+          duration: 5000,
+          isClosable: true,
+        });
+      } else {
+        toast({
+          title: `${selectedUser}'s password successfully reset.`,
+          status: 'success',
+          duration: 5000,
+          isClosable: true,
+        });
+        onModalClose();
+      }
+    });
+  };
 
   return (
-    <Box minH='100vh' h='100vh'>
-      <Flex h='100%' flexFlow='column'>
+    <Box minH="100vh" h="100vh">
+      <Flex h="100%" flexFlow="column">
         <NavigationBar />
         <Heading>Admin Dashboard</Heading>
-        <SimpleGrid columns={[1, 2, 3, 4, 5]} spacing='3' m='3'>
+        <SimpleGrid columns={[1, 2, 3, 4, 5]} spacing="3" m="3">
           {users.map((user, index) => {
             return (
-              <Box bg='blue.50' key={index} borderRadius='xl' p='2'>
-                <Text as='b'>{user.email}</Text>
-                  <ButtonGroup size='sm' isAttached>
-                    <Button bg={'red.400'} color={'white'} _hover={{ bg: 'red.500' }} onClick={() => handleDeleteButton(user.username, user.email)}>Delete</Button>
-                    <Button bg={'blue.400'} color={'white'} _hover={{ bg: 'blue.500' }} onClick={() => handleResetButton(user.username, user.email)}>Reset Password</Button>
-                  </ButtonGroup>
+              <Box bg="blue.50" key={index} borderRadius="xl" p="2">
+                <Text as="b">{user.email}</Text>
+                <ButtonGroup size="sm" isAttached>
+                  <Button
+                    bg={'red.400'}
+                    color={'white'}
+                    _hover={{ bg: 'red.500' }}
+                    onClick={() =>
+                      handleDeleteButton(user.username, user.email)
+                    }
+                  >
+                    Delete
+                  </Button>
+                  <Button
+                    bg={'blue.400'}
+                    color={'white'}
+                    _hover={{ bg: 'blue.500' }}
+                    onClick={() => handleResetButton(user.username, user.email)}
+                  >
+                    Reset Password
+                  </Button>
+                </ButtonGroup>
               </Box>
-            )
+            );
           })}
         </SimpleGrid>
       </Flex>
-      <AlertDialog
-        isOpen={isAlertOpen}
-        onClose={onAlertClose}
-      >
+      <AlertDialog isOpen={isAlertOpen} onClose={onAlertClose}>
         <AlertDialogOverlay>
           <AlertDialogContent>
-            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
               Delete {selectedUser}&rsquo;s Account
             </AlertDialogHeader>
             <AlertDialogBody>
               Are you sure? You cannot undo this action afterwards.
             </AlertDialogBody>
             <AlertDialogFooter>
-              <Button onClick={onAlertClose}>
-                Cancel
-              </Button>
-              <Button colorScheme='red' onClick={handleFinalDelete} ml={3}>
+              <Button onClick={onAlertClose}>Cancel</Button>
+              <Button colorScheme="red" onClick={handleFinalDelete} ml={3}>
                 Delete
               </Button>
             </AlertDialogFooter>
@@ -175,24 +226,36 @@ const AdminDashboard = () => {
             <Stack spacing={4}>
               <FormControl>
                 <FormLabel>New Password:</FormLabel>
-                <PasswordBar value={newPassword} onChange={(event) => setNewPassword(event.target.value)} />
+                <PasswordBar
+                  value={newPassword}
+                  onChange={event => setNewPassword(event.target.value)}
+                />
               </FormControl>
               <FormControl>
                 <FormLabel>Confirm Password:</FormLabel>
-                <PasswordBar value={confirmNewPassword} onChange={(event) => setConfirmNewPassword(event.target.value)} />
+                <PasswordBar
+                  value={confirmNewPassword}
+                  onChange={event => setConfirmNewPassword(event.target.value)}
+                />
               </FormControl>
             </Stack>
           </ModalBody>
 
           <ModalFooter>
-              <Button loadingText="Submitting" bg={'blue.400'} color={'white'} _hover={{ bg: 'blue.500' }} onClick={handleFinalPasswordReset}>
-                Submit
-              </Button>
+            <Button
+              loadingText="Submitting"
+              bg={'blue.400'}
+              color={'white'}
+              _hover={{ bg: 'blue.500' }}
+              onClick={handleFinalPasswordReset}
+            >
+              Submit
+            </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
     </Box>
   );
-}
+};
 
 export default AdminDashboard;
--- src/frontend/src/pages/ForgotPassword.jsx
+++ src/frontend/src/pages/ForgotPassword.jsx
@@ -11,10 +11,11 @@ import {
   Text,
 } from '@chakra-ui/react';
 import PasswordBar from '../components/PasswordBar/PasswordBar';
-import { fetchBackend } from '../fetch';
+// import { fetchBackend } from '../fetch';
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [emailError, setEmailError] = useState(false);
@@ -22,44 +23,69 @@ const ForgotPassword = () => {
   const [confirmPasswordError, setConfirmPasswordError] = useState(false);
   const [passwordChanged, setPasswordChanged] = useState(false);
 
-  const handleSubmit = () => {
-    // Get user accounts from local storage
-    const userAccounts = JSON.parse(localStorage.getItem('userAccounts')) || [];
+  const handleSubmit = async () => {
+    // // Get user accounts from local storage
+    // const userAccounts = JSON.parse(localStorage.getItem('userAccounts')) || [];
 
-    // Find the user with matching email
-    const user = userAccounts.find(user => user.email === email);
-
-    if (user) {
-      if (password === confirmPassword) {
-        const token = localStorage.getItem('token');
-        fetchBackend('/update/password', 'PUT', { token, password }).then(
-          data => {
-            if (data.error) {
-              setPasswordError(true);
-              setConfirmPasswordError(true);
-              setPasswordChanged(false);
-              console.error('Password and Confirm Password do not match');
-            } else {
-              // Password change successful
-              user.password = password; // Update the password in the user object
-              localStorage.setItem(
-                'userAccounts',
-                JSON.stringify(userAccounts)
-              ); // Update the user accounts in local storage
-              setEmailError(false);
-              setPasswordError(false);
-              setConfirmPasswordError(false);
-              setPasswordChanged(true);
-              console.log('Password changed successfully');
-            }
-          }
-        );
+    // // Find the user with matching email
+    // const user = userAccounts.find(user => user.email === email);
+    if (password === confirmPassword) {
+      try {
+        const response = await fetch('http://127.0.0.1:5000/reset/password', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(username, email),
+        });
+        if (response.ok) {
+          setEmailError(false);
+          setPasswordError(false);
+          setConfirmPasswordError(false);
+          setPasswordChanged(true);
+          console.log('Password changed successfully');
+        } else {
+          setEmailError(true);
+          setPasswordChanged(false);
+          console.error('Email or Username does not exist');
+        }
+      } catch (error) {
+        console.error('An error occurred while logging in', error);
       }
     } else {
-      setEmailError(true);
+      setPasswordError(true);
+      setConfirmPasswordError(true);
       setPasswordChanged(false);
-      console.error('Email does not exist');
+      console.error('Password and Confirm Password do not match');
     }
+    // if (password === confirmPassword) {
+    //     const token = localStorage.getItem('token');
+    //     fetchBackend('/update/password', 'PUT', { token, password }).then(
+    //       data => {
+    //         if (data.error) {
+    //           setPasswordError(true);
+    //           setConfirmPasswordError(true);
+    //           setPasswordChanged(false);
+    //           console.error('Password and Confirm Password do not match');
+    //         } else {
+    //           // Password change successful
+    //           user.password = password; // Update the password in the user object
+    //           localStorage.setItem(
+    //             'userAccounts',
+    //             JSON.stringify(userAccounts)
+    //           ); // Update the user accounts in local storage
+    //           setEmailError(false);
+    //           setPasswordError(false);
+    //           setConfirmPasswordError(false);
+    //           setPasswordChanged(true);
+    //           console.log('Password changed successfully');
+    //         }
+    //       }
+    //     );
+    //   }
+    // } else {
+
+    // }
   };
 
   return (
@@ -82,6 +108,14 @@ const ForgotPassword = () => {
         <Heading lineHeight={1.1} fontSize={{ base: '2xl', md: '3xl' }}>
           Enter new password
         </Heading>
+        <FormControl id="username" isRequired>
+          <FormLabel>Username</FormLabel>
+          <Input
+            type="text"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
+        </FormControl>
         <FormControl id="email" isRequired>
           <FormLabel>Email address</FormLabel>
           <Input
@@ -94,7 +128,7 @@ const ForgotPassword = () => {
         </FormControl>
         {emailError && (
           <Text color="red.500" fontSize="sm">
-            The provided email does not exist.
+            The provided email or username does not exist.
           </Text>
         )}
         <FormControl id="password" isRequired>
