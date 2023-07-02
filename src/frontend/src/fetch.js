0fef2f636c880983e89ad5b91371790f452c29ef
Jonathan Sinani <z5256969@unsw.edu.au>
2023-07-03 01:00:54 +1000

    Added error message to fetch backend function, and function to run on success

--- src/frontend/src/fetch.js
+++ src/frontend/src/fetch.js
@@ -2,7 +2,7 @@ export const isNone = (value) => value === null || value === undefined;
 
 // Fetches with the route, method, body (optional). alerts and setAlerts are required if an error message is to
 // be displayed. Token is not reqired but routes that need a token will give an error.
-export const fetchBackend = async (route, method, body) => {
+export const fetchBackend = async (route, method, body, toast, onSuccess) => {
   const options = {
     method,
     headers: {
@@ -19,5 +19,32 @@ export const fetchBackend = async (route, method, body) => {
 
   const data = await response.json();
 
-  return data;
+  let isSuccess = false;
+  if (!isNone(data.Success)) {
+    isSuccess = data.Success;
+  } else {
+    isSuccess = data.Succes;
+  }
+
+  if (!isSuccess) {
+    let errorMessage = 'Error!';
+    if (!isNone(data.Error)) {
+      errorMessage = data.Error;
+    } else if (!isNone(data.Message)) {
+      errorMessage = data.Message;
+    }
+    if (isNone(toast)) {
+      toast({
+        title: errorMessage,
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+    return
+  }
+
+  if (!isNone(onSuccess)) {
+    onSuccess();
+  }
 }
