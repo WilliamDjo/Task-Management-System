d278870001849eea0b54b54944e386df652c1ffb
sanyamjain47 <sanyamjain47@gmail.com>
2023-07-03 23:11:55 +1000

    Added code for reset password

--- src/backend/password.py
+++ src/backend/password.py
@@ -13,34 +13,34 @@ from database import db
 
 reset_otps = []
 
+
 def reset_password(email):
     # Check if email exists
     email_exists = db.checkUser(email)
 
     if email_exists["Success"]:
         return {"Success": False, "Message": "Email doesn't exist"}
-    
+
     for i in reset_otps:
-        if i['email'] == email:
-            return {"Success" : False, "Message": "OTP already sent"}
+        if i["email"] == email:
+            return {"Success": False, "Message": "OTP already sent"}
 
     random_number = random.randint(100000, 999999)
     send_email(email, random_number)
-    reset_otps.append({
-        'email':email,
-        'otp':random_number
-    })
+    reset_otps.append({"email": email, "otp": random_number})
+
 
 def check_otp(email, otp):
     temp = None
     for i in reset_otps:
-        if i['email'] == email and i['otp'] == otp:
+        if i["email"] == email and i["otp"] == otp:
             temp = i
     if temp is not None:
         reset_otps.remove(temp)
-        return {"Success":True, "Message":""}
+        return {"Success": True, "Message": ""}
     else:
-        return {"Success":False, "Message":"OTP did not match"}
+        return {"Success": False, "Message": "OTP did not match"}
+
 
 def send_email(email, otp):
     # Create a new thread to send the email
@@ -51,17 +51,17 @@ def send_email(email, otp):
 
         # Creating the message
         msg = MIMEMultipart()
-        msg['From'] = username
-        msg['To'] = email
-        msg['Subject'] = "Your OTP"
+        msg["From"] = username
+        msg["To"] = email
+        msg["Subject"] = "Your OTP"
 
         # The actual message
         message = f"Your OTP is {otp}"
-        msg.attach(MIMEText(message, 'plain'))
+        msg.attach(MIMEText(message, "plain"))
 
         # Login and send the email
         try:
-            server = smtplib.SMTP('smtp.example.com', 587)
+            server = smtplib.SMTP("smtp.example.com", 587)
             server.starttls()
             server.login(username, password)
             text = msg.as_string()
