4786ad106d344807051b605a226f344f15c39e36
Jonathan Sinani <z5256969@unsw.edu.au>
2023-07-12 12:00:46 +1000

    Adding connections frontend functionality is done

--- src/frontend/src/pages/AddConnection.jsx
+++ src/frontend/src/pages/AddConnection.jsx
@@ -1,15 +1,79 @@
+import { ArrowBackIcon } from '@chakra-ui/icons';
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Link,
+  Show,
+  Stack,
+  useToast,
+} from '@chakra-ui/react';
 import React from 'react';
+import { Link as RouteLink } from 'react-router-dom';
+
 import ConnectionsBar from '../components/ConnectionsBar';
-import { Box, Button, Heading, Input } from '@chakra-ui/react';
+import { fetchBackend } from '../fetch';
 
 const AddConnection = () => {
   const [email, setEmail] = React.useState('');
+
+  const toast = useToast();
+
+  const handleRequestConnection = () => {
+    const successRequestConnection = () => {
+      toast({
+        title: 'Request sent.',
+        status: 'success',
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+    const token = localStorage.getItem('token');
+    const body = {
+      token,
+      email
+    }
+    fetchBackend('/user/addconnection', 'POST', body, toast, successRequestConnection);
+  }
   return (
     <ConnectionsBar addConnections>
       <Box>
-        <Heading>Add Connections</Heading>
-        <Input value={email} onChange={(event) => setEmail(event.target.value)}></Input>
-        <Button>Send</Button>
+      <Show below='sm'>
+        <Link as={RouteLink} to='/connections'>
+          <Flex align='center'>
+            <ArrowBackIcon/>
+            Back to Connections
+          </Flex>
+        </Link>
+      </Show>
+        <Heading>
+          Add Connection
+        </Heading>
+        <Stack spacing="4">
+          <Box>
+            <FormControl>
+              <FormLabel>New Connection Email:</FormLabel>
+              <Input
+                placeholder="email@email.com"
+                value={email}
+                onChange={event => setEmail(event.target.value)}
+              />
+            </FormControl>
+          </Box>
+          <Button
+            loadingText="Submitting"
+            bg={'blue.400'}
+            color={'white'}
+            _hover={{ bg: 'blue.500' }}
+            onClick={handleRequestConnection}
+          >
+            Send Request
+          </Button>
+        </Stack>
       </Box>
     </ConnectionsBar>
   );
