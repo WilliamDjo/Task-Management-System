94e302ef5601827fcf79687f2fea6ab4093af3d6
Sanyam Jain <sanyamjain47@gmail.com>
2023-07-19 12:17:15 +1000

    Fixed tasks create

--- src/backend/tests/full_test.py
+++ src/backend/tests/full_test.py
@@ -22,6 +22,15 @@ user_2 = {
     "email": "sanyamjain2@gmail.com",
 }
 
+admin_1 = {
+    "username": "sanyamjain2",
+    "password": "Testpass123",
+    "first_name": "Admin",
+    "last_name": "Admin",
+    "sys_admin": True,
+    "email": "adminer@admin.com",
+}
+
 # # test user registration
 # response = requests.post(
 #     f"{BASE_URL}/signup",
@@ -30,6 +39,10 @@ user_2 = {
 # resp_json = response.json()
 # print(resp_json)
 # token = resp_json["Token"]
+# headers = {
+#     "Authorization": f"Bearer {token}",
+# }
+
 # response = requests.post(f"{BASE_URL}/logout", json={"token": token})
 # resp_json = response.json()
 # print(resp_json)
@@ -42,6 +55,9 @@ user_2 = {
 # resp_json = response.json()
 # print(resp_json)
 # token = resp_json["Token"]
+# headers = {
+#     "Authorization": f"Bearer {token}",
+# }
 
 # # test logout
 # response = requests.post(f"{BASE_URL}/logout", json={"token": token})
@@ -54,6 +70,31 @@ user_2 = {
 # resp_json = response.json()
 # print(resp_json)
 # token = resp_json["Token"]
+# headers = {
+#     "Authorization": f"Bearer {token}",
+# }
+
+# headers = {
+#     "Authorization": f"Bearer {token}",
+# }
+
+# response = requests.post(f"{BASE_URL}/logout", json={"token": token})
+# resp_json = response.json()
+# print(resp_json)
+
+
+# # test user registration
+# response = requests.post(
+#     f"{BASE_URL}/signup",
+#     json=admin_1,
+# )
+# resp_json = response.json()
+# print(resp_json)
+# token = resp_json["Token"]
+# headers = {
+#     "Authorization": f"Bearer {token}",
+# }
+
 # response = requests.post(f"{BASE_URL}/logout", json={"token": token})
 # resp_json = response.json()
 # print(resp_json)
@@ -66,6 +107,9 @@ user_2 = {
 # resp_json = response.json()
 # print(resp_json)
 # token = resp_json["Token"]
+# headers = {
+#     "Authorization": f"Bearer {token}",
+# }
 
 # # test logout
 # response = requests.post(f"{BASE_URL}/logout", json={"token": token})
@@ -81,7 +125,41 @@ print(resp_json)
 headers = {
     "Authorization": f"Bearer {token}",
 }
-
+print(headers)
 response = requests.post(f"{BASE_URL}/getuserprofile", headers=headers)
+resp_json = response.json()
+print(resp_json)
+
+data_1 = {
+    "title": "For Demo2",
+    "description": "Task Description",
+    "deadline": "2023-31-07",
+    "progress": "Not Started",
+    "cost_per_hr": 10,
+    "assignee": "sanyamjain1@gmail.com",
+    "estimation_spent_hrs": 0,
+    "actual_time_hr": 0,
+    "priority": 2,
+    "labels": ["Label1", "Label2"],
+}
+data_2 = {
+    "title": "For Demo2",
+    "description": "Task Description",
+    "deadline": "2023-31-07",
+    "progress": "Not Started",
+    "cost_per_hr": 10,
+    "assignee": "sanyamjain2@gmail.com",
+    "estimation_spent_hrs": 0,
+    "actual_time_hr": 0,
+    "priority": 2,
+    "labels": ["Label1", "Label2"],
+}
+
+response = requests.post(f"{BASE_URL}/task/create", headers=headers, json=data_1)
+
+resp_json = response.json()
+print(resp_json)
+response = requests.post(f"{BASE_URL}/task/create", headers=headers, json=data_2)
+
 resp_json = response.json()
 print(resp_json)
