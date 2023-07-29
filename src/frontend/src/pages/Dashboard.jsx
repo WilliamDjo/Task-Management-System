f3f5ca7471416e384f46e2c57aaf228e355bccff
Jonathan Sinani <z5256969@unsw.edu.au>
2023-07-29 10:46:24 +1000

    Added the json conversion used in get all tasks to the get task by id , get assigned to task and get given by task. Not sure if this is correct.

--- src/frontend/src/pages/Dashboard.jsx
+++ src/frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Box, Button, Flex, useToast } from '@chakra-ui/react';
+import { Box, Button, Flex, FormControl, FormLabel, Input, useToast } from '@chakra-ui/react';
 import NavigationBar from '../components/NavigationBar';
 import KanbanBoard from '../components/KanbanBoard';
 import { fetchBackend } from '../fetch';
 
 const Dashboard = () => {
+  const [tasks, setTasks] = React.useState([
+    {
+      title: 'Hello'
+    }
+  ]);
+  const [email, setEmail] = React.useState('');
   const toast = useToast();
 
   const newTask = () => {
@@ -40,17 +46,70 @@ const Dashboard = () => {
     fetchBackend('/task/create', 'POST', body, toast, success);
   }
 
-  React.useEffect(() => {
+  const showTasks = () => {
+    const success = (data) => {
+      setTasks(data.Data);
+    }
+
+    const token = localStorage.getItem('token');
+    fetchBackend('/task/getAll', 'GET', { token }, toast, success);
+  }
+
+  const consoleLogTask = (id) => {
     const success = (data) => {
       console.log(data);
     }
+
+    const token = localStorage.getItem('token');
+    fetchBackend(`/task/get/${id}`, 'GET', { token }, toast, success);
+  }
+
+  const tasksAssigned = () => {
+    const success = (data) => {
+      console.log(data);
+    }
+
+    const token = localStorage.getItem('token');
+    fetchBackend(`/task/getAllAssignedTo/${email}`, 'GET', { token }, toast, success);
+  }
+
+  const tasksGiven = () => {
+    const success = (data) => {
+      console.log(data);
+    }
+
+    const token = localStorage.getItem('token');
+    fetchBackend(`/task/getTasksGivenBy/${email}`, 'GET', { token }, toast, success);
+  }
+
+  React.useEffect(() => {
+    const success = (data) => {
+      console.log(data);
+      setTasks(data.Data)
+    }
     const token = localStorage.getItem('token');
     fetchBackend('/task/getAll', 'GET', { token }, toast, success);
-  });
+  }, []);
   return (
     <Box minH="100vh" h="100vh">
       <Flex h="100%" flexFlow="column">
-        <Button onClick={newTask}>Click here</Button>
+        <Button onClick={newTask}>Click here for new tasks</Button>
+        <Button onClick={showTasks}>Click here to show all tasks</Button>
+        <FormControl>
+          <FormLabel>Email:</FormLabel>
+          <Input value={email} onChange={(e) => setEmail(e.target.value)}></Input>
+        </FormControl>
+        <Button onClick={tasksAssigned}>Click here to console log all tasks assigned to email</Button>
+        <Button onClick={tasksGiven}>Click here to console log all tasks given by email</Button>
+        <Box>
+          {tasks.map((x, index) => {
+            return (
+              <Box key={index} onClick={() => consoleLogTask(x.id)}>
+                {x.title} - {x.id}
+              </Box>
+            )
+          })}
+        </Box>
         <NavigationBar />
         {/* <ChakraProvider> */}
         <KanbanBoard />
