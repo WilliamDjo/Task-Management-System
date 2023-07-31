6586048a5f2a5c6e54d0d8343b2e04d961aa32ee
ax67 <71861302+ax67@users.noreply.github.com>
2023-07-31 14:51:18 +1000

    workload changes - added db

--- src/database/db.py
+++ src/database/db.py
@@ -24,6 +24,7 @@ def addNewUser(data: dict) -> dict:
         "first_name": "",
         "last_name": "",
         "SystemAdmin": False,
+        "workload": 0,
     }
 
     # Loop over the keys in the input data
