174a99586ded5298a3e74062d1f7dd87a2681746
Jonathan Sinani <z5256969@unsw.edu.au>
2023-08-01 20:50:55 +1000

    Moved chat into it's own component that is still part of the tabs in the connection page

--- src/frontend/src/pages/ConnectionProfile.jsx
+++ src/frontend/src/pages/ConnectionProfile.jsx
@@ -4,16 +4,9 @@ import {
   Card,
   CardBody,
   Center,
-  Divider,
-  Flex,
   Heading,
   Hide,
-  IconButton,
-  Input,
-  InputGroup,
-  InputRightElement,
   Spinner,
-  Stack,
   Tab,
   TabList,
   TabPanel,
@@ -23,7 +16,6 @@ import {
   Tabs,
   Tbody,
   Td,
-  Text,
   Th,
   Thead,
   Tr,
@@ -34,7 +26,7 @@ import { useParams } from 'react-router-dom';
 import ConnectionsBar from '../components/ConnectionsBar';
 import { fetchBackend } from '../fetch';
 import ProfileCard from '../components/ProfileCard';
-import { EmailIcon } from '@chakra-ui/icons';
+import ConnectionChat from './ConnectionChat';
 
 const ConnectionProfile = () => {
   const [name, setName] = React.useState('Name');
@@ -49,22 +41,6 @@ const ConnectionProfile = () => {
     },
   ]);
 
-  const [messages, setMessages] = React.useState([
-    {
-      message: 'Hello there',
-      timestamp: '2023-07-29 12:00:00PM',
-      sender: true,
-      clicked: false
-    },
-    {
-      message: 'Hi!',
-      timestamp: '2023-07-29 12:01:00PM',
-      sender: false,
-      clicked: false
-    }
-  ])
-  const [chatInput, setChatInput] = React.useState('');
-
   const { email } = useParams();
 
   const toast = useToast();
@@ -73,21 +49,13 @@ const ConnectionProfile = () => {
 
   React.useEffect(() => {
     const token = localStorage.getItem('token');
-    const successGetChat = (data) => {
-      const chats = data.Data.map((chat) => {
-        chat.clicked = false;
-        return chat;
-      })
-      setMessages(chats);
-      setLoaded(true);
-    }
 
     const successGetConnectionProfile = (data) => {
       setName(`${data.Data.first_name} ${data.Data.last_name}`);
       setUsername(data.Data.username);
       setWorkload(data.Data.workload);
       setTasks(data.Tasks);
-
+      setLoaded(true);
     };
 
     fetchBackend(
@@ -97,18 +65,6 @@ const ConnectionProfile = () => {
       toast,
       successGetConnectionProfile
     );
-
-    // const id = setInterval(() => {
-    //   fetchBackend(
-    //     `/chat/${email}`,
-    //     'GET',
-    //     { token },
-    //     toast,
-    //     successGetChat
-    //   )
-    // }, 30000);
-
-    // return () => clearInterval(id);
   }, []);
 
   const connectionProfileLoaded = () => {
@@ -120,7 +76,6 @@ const ConnectionProfile = () => {
         workload={workload}
       />
     );
-
   };
 
   const connectionAssignedTaskListLoaded = () => {
@@ -161,101 +116,6 @@ const ConnectionProfile = () => {
     );
   };
 
-  const messageClick = (index) => {
-    const newMessages = [...messages];
-    newMessages[index].clicked = !newMessages[index].clicked;
-    setMessages(newMessages);
-  }
-
-  const sendMessage = () => {
-    const successSendMessage = (data) => {
-      const newMessages = [...messages];
-      newMessages.unshift({
-        message: chatInput,
-        timestamp: data.Timestamp,
-        sender: true,
-        clicked: false
-      });
-      setMessages(newMessages);
-      setChatInput('');
-    }
-
-    if (chatInput === '') {
-      toast({
-        title: 'Message must have content',
-        status: 'warning',
-        duration: 5000,
-        isClosable: true,
-      });
-      return;
-    }
-    const token = localStorage.getItem('token');
-    const body = {
-      token,
-      message: chatInput
-    }
-    fetchBackend(
-      `/chat/${email}`,
-      'POST',
-      body,
-      toast,
-      successSendMessage,
-      successSendMessage
-    );
-  }
-
-  const chatCardLoaded = () => {
-    return (
-      <Card mt="4">
-        <CardBody>
-          <Stack spacing='4'>
-            <Heading fontSize="lg" textTransform="uppercase">
-              Chat
-            </Heading>
-            <Divider />
-            <InputGroup>
-              <Input
-                value={chatInput}
-                onChange={(event) => setChatInput(event.target.value)}
-                onKeyDown={event => {
-                  if (event.key === 'Enter') {
-                    sendMessage();
-                    event.target.blur();
-                  }
-                }}
-              />
-              <InputRightElement>
-                <IconButton
-                  isRound={true}
-                  bg='blue.400'
-                  color='white'
-                  _hover={{ bg: 'blue.500' }}
-                  size='sm'
-                  icon={<EmailIcon />}
-                  onClick={sendMessage}
-                />
-              </InputRightElement>
-            </InputGroup>
-            <Stack spacing='4'>
-              {messages.map((message, index) => {
-                return (
-                  <Flex justify={message.sender ? 'right' : 'left'} key={index}>
-                    <Stack>
-                      <Box bg={message.sender ? 'blue.200' : 'gray.200'} p='4' rounded='full' minW='32' onClick={() => messageClick(index)}>
-                        <Text>{message.message}</Text>
-                      </Box>
-                      {message.clicked && <Box rounded='full' bg='white'><Text fontSize='2xs'>{message.timestamp}</Text></Box>}
-                    </Stack>
-                  </Flex>
-                );
-              })}
-            </Stack>
-          </Stack>
-        </CardBody>
-      </Card>
-    )
-  }
-
   return (
     <ConnectionsBar myConnections>
       <Box>
@@ -270,7 +130,7 @@ const ConnectionProfile = () => {
               {loaded ? connectionProfileLoaded() : (<Center><Spinner /></Center>)}
             </TabPanel>
             <TabPanel>
-              {loaded ? chatCardLoaded() : (<Center><Spinner /></Center>)}
+              <ConnectionChat email={email} />
             </TabPanel>
             <TabPanel>
               {loaded ? connectionAssignedTaskListLoaded() : (<Center><Spinner /></Center>)}
