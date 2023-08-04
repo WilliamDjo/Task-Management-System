fd887cfdb0ecd05ce22b35053c689f8bdba2ca1a
Sanyam Jain <sanyamjain47@gmail.com>
2023-08-04 20:25:42 +1000

    Done

--- src/build.sh
+++ src/build.sh
@@ -5,3 +5,9 @@ docker exec src-mongodb-1 mongoimport --host mongodb --db TaskSystem --collectio
 docker exec src-mongodb-1 mongoimport --host mongodb --db TaskSystem --collection task_chats --type json --file /docker-entrypoint-initdb.d/TaskSystem.task_chats.json --jsonArray
 docker exec src-mongodb-1 mongoimport --host mongodb --db TaskSystem --collection user_info --type json --file /docker-entrypoint-initdb.d/TaskSystem.user_info.json --jsonArray
 docker exec src-mongodb-1 mongoimport --host mongodb --db TaskSystem --collection user_profile --type json --file /docker-entrypoint-initdb.d/TaskSystem.user_profile.json --jsonArray
+
+# wait for "Compiled successfully!" message in src-frontend-1 logs
+docker logs -f src-frontend-1 2>&1 | grep -m 1 "Compiled successfully!"
+
+# print Ready
+echo "Ready. Please, go to 'localhost:3000' on your browser."
